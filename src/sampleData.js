dccc349aa661522c3d26c1cfa2d328e361c1387a
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-07-10 21:46:29 -0400

    sets up sample data

---
+++ src/sampleData.js
@@ -0,0 +1,37 @@
+export const sampleData = [
+  {
+    listName: "Went Well",
+    listContents: [
+      {
+        itemText: "I coded my first React component",
+        itemUpvoteCount: 0,
+        itemDownvoteCount: 0,
+      },
+    ],
+  },
+  {
+    listName: "To Improve",
+    listContents: [
+      {
+        itemText: "Life",
+        itemUpvoteCount: 0,
+        itemDownvoteCount: 0,
+      },
+      {
+        itemText: "Thinking more positively",
+        itemUpvoteCount: 0,
+        itemDownvoteCount: 0,
+      },
+    ],
+  },
+  {
+    listName: "Action Items",
+    listContents: [
+      {
+        itemText: "Completing this",
+        itemUpvoteCount: 0,
+        itemDownvoteCount: 0,
+      },
+    ],
+  },
+];
