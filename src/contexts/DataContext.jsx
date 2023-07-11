15f9b7cfb7ae2f0a048b05f202d9c97c2b495fa2
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-07-11 16:55:38 -0400

    fixes syntax error in DataProvider

--- src/contexts/DataContext.jsx
+++ src/contexts/DataContext.jsx
@@ -3,10 +3,10 @@ import { useState, useContext, createContext } from "react";
 import { sampleData } from "../sampleData";
 export const DataContext = createContext();
 
-export function DataProvider = ({children}) => {
+export const DataProvider = ({children}) => {
   const [data, setData] = useState(sampleData);
 
-  
+
   return (<DataContext.Provider value={ }>
     {children}
   </DataContext.Provider>)
