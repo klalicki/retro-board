866967debdf8c52e98e4feb131ba334a1f3b15e0
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-07-12 20:11:53 -0400

    adds basic markup for button icons on Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRight,
+  faXmark,
+  faArrowLeft,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Card = (props) => {
   return (
     <div className="card">
       <h2>Card: {props.cardText}</h2>
       {props.children}
+      <div className="icon-bar">
+        <FontAwesomeIcon icon={faArrowLeft} />
+        <FontAwesomeIcon icon={faXmark} />
+        <FontAwesomeIcon icon={faArrowRight} />
+      </div>
     </div>
   );
 };
