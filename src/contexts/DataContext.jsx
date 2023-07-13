c880274d3d44bd00c787367c5989aeb3f830831a
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-07-12 20:45:49 -0400

    adds list of functions to implement in Context

--- src/contexts/DataContext.jsx
+++ src/contexts/DataContext.jsx
@@ -5,7 +5,12 @@ export const DataContext = createContext();
 
 export const DataProvider = ({children}) => {
   const [data, setData] = useState(sampleData);
+/* list of functions to implement
+addItem(item)
+removeItem(item)
+moveItem(direction)
 
+*/
 
   return (<DataContext.Provider value={ }>
     {children}
