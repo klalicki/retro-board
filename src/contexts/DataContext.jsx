0a941c742f23e7e3ca9c4b3c74a405e668cac739
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-07-14 22:10:33 -0400

    adds function to add item to data

--- src/contexts/DataContext.jsx
+++ src/contexts/DataContext.jsx
@@ -3,17 +3,30 @@ import { useState, useContext, createContext } from "react";
 import { sampleData } from "../sampleData";
 export const DataContext = createContext();
 
-export const DataProvider = ({children}) => {
+export const DataProvider = ({ children }) => {
   const [data, setData] = useState(sampleData);
-/* list of functions to implement
+  /* list of functions to implement
 addItem(item)
 removeItem(item)
 moveItem(direction)
 
 */
+  const addItem = (text, column) => {
+    const itemId = Date.now();
+    const newItem = {
+      itemId: itemId,
+      itemText: text,
+      itemUpvoteCount: 0,
+      itemDownvoteCount: 0,
+    };
+    const newData = data;
+    newData[column].push(newItem);
+    setData(newData);
+  };
 
-  return (<DataContext.Provider value={ }>
-    {children}
-  </DataContext.Provider>)
-
-}
+  return (
+    <DataContext.Provider value={(data, addItem)}>
+      {children}
+    </DataContext.Provider>
+  );
+};
