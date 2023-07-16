0a0712fb6e1c3a91f5ab05d55017e3561bef3f1b
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-07-15 23:16:55 -0400

    adds "plus" button to CardList component

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Card from "../Card/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const CardList = (props) => {
   return (
     <div className="card-list">
-      <h1>
-        CardList {props.listIndex}: {props.listName}
-      </h1>
+      <article className="card-list-header">
+        <h1>
+          CardList {props.listIndex}: {props.listName}
+        </h1>
+        <button>
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </article>
+
       {props.items.map((listItem) => {
         return <Card cardText={listItem.itemText}></Card>;
       })}
