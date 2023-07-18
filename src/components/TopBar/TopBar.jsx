f0c423df9e26fc5728065386b26d1c8a403b1e10
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-07-18 00:06:43 -0400

    implements basic localStorage save

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { useContext } from "react";
+import { DataContext } from "../../contexts/DataContext";
 import { ModeContext } from "../../contexts/ModeContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faSun,
   faMoon,
+  faFloppyDisk,
+  faFolderOpen,
   faChartSimple,
 } from "@fortawesome/free-solid-svg-icons";
 
 const TopBar = ({ children }) => {
   const { mode, toggleLayout, setDarkMode } = useContext(ModeContext);
+  const { saveToLocalStorage, loadFromLocalStorage } = useContext(DataContext);
   return (
     <header className="header">
       <h2>Retro Board</h2>
@@ -37,6 +41,20 @@ const TopBar = ({ children }) => {
           <FontAwesomeIcon icon={faChartSimple} rotation={90} />
         )}{" "}
         switch layout
+      </button>
+      <button
+        onClick={() => {
+          saveToLocalStorage();
+        }}
+      >
+        <FontAwesomeIcon icon={faFloppyDisk} />
+      </button>
+      <button
+        onClick={() => {
+          loadFromLocalStorage();
+        }}
+      >
+        <FontAwesomeIcon icon={faFolderOpen} />
       </button>
     </header>
   );
